9f4fc3d1913d726ff421752f636098fc3795e188
Anthony Lucas <AnthonyJLucas@gmail.com>
2012-06-18 11:30:18 +0100

    fixed some compilation errors

--- lib/Config/YAARG.pm
+++ lib/Config/YAARG.pm
@@ -5,29 +5,35 @@
 
 
 
-package Config::YAARG v0.0.1;
+package Config::YAARG 0.01;
 use base qw( Exporter );
 
 
 
 use strict;
 use warnings;
+use feature 'switch';
+
+
+
+use Class::ISA ( );
+use Getopt::Long ( );
 
 
 
 #EXPORT CONFIGURATION
 
 
-our @PUBLIC_CONSTANTS => qw/
+our @PUBLIC_CONSTANTS = qw/
     ARG_PASSTHROUGH
     ARG_IGNORE
-    ARG_VALUE_MAP
+    ARG_VALUE_TRANS
     ARG_NAME_MAP
 /;
 
-our @SCRIPT_ROUTINES => qw/ARGS/;
-our @CLASS_METHODS => qw/process_args/;
-our @STANDARD_ROUTINES => qw/ProcessArgs/;
+our @SCRIPT_ROUTINES = qw/ARGS/;
+our @CLASS_METHODS = qw/process_args/;
+our @STANDARD_ROUTINES = qw/ProcessArgs/;
 
 our @EXPORT_OK = (
     @PUBLIC_CONSTANTS,
@@ -51,12 +57,111 @@ use constant ARG_IGNORE => 'ignore';
 
 
 sub ARG_NAME_MAP {};
-sub ARG_VALUE_MAP {};
+sub ARG_VALUE_TRANS {};
+
+
+
+#SCRIPT HELPER ROUTINE
+
+
+sub ARGS {
+
+    my $class = caller();
+    my $config = _yaarg_fetch_config($class);
+    my $names = $config->{names};
+    return unless ($names);
+
+    my %args = ();
+    Getopt::Long::GetOptions(\%args,
+        map { !/=/ ? "$_=s" : $_ } @$names);
+    return %{ProcessArgs(__PACKAGE__, $config, %args)};
+}
+
+
+
+#CLASS HELPER ROUTINE
+
+
+sub process_args {
+
+    my ($self, @args) = @_;
+
+    #detect alt call signatures
+    my $target = (@args % 2)
+        ? shift(@args)
+        : {};
+
+    #gather config and process args
+    my $result = $self->ProcessArgs(
+        $self->_yaarg_fetch_config,
+        @args);
+
+    #copy results to target struct
+    $target->{$_} = $result->{$_}
+        foreach (keys(%{$result}));
+
+    return $target;
+}
 
 
 
 #CORE ROUTINES
 
+
+sub ProcessArgs {
+
+    my ($class, $config, %args) = @_;
+
+    my $map = $config->{'map'};
+    my $trans = $config->{'trans'};
+
+    my $t_args;
+    $t_args = $class->_yaarg_transform_values(\%args, $trans)
+        if ($trans);
+    $t_args = $class->_yaarg_transform_keys($t_args, $map)
+        if ($map);
+    return $t_args || {};
+}
+
+
+sub _yaarg_fetch_config {
+
+    my ($context, $class) = @_;
+    $class ||= $context;
+
+    my @ISA = Class::ISA::self_super_path($class);
+    my (@map, @trans, @names);
+    foreach (@ISA) {
+        my ($m, $t, $n) = $context
+            ->_yaarg_fetch_class_config($_);
+        push(@map, _yaarg_to_list($m));
+        push(@trans, _yaarg_to_list($t));
+        push(@names, __yaarg_to_list($n));
+    }
+    return {
+        map => {@map},
+        trans => {@trans},
+        names => \@names
+    };
+}
+
+
+sub _yaarg_fetch_class_config {
+
+    my $class = $_[1];
+    my @return;
+
+    foreach (qw/
+        ARG_NAME_MAP
+        ARG_VALUE_TRANS
+        ARG_NAME_LIST/) {
+
+        push(@return, (($class->can($_))
+            ? $class->$_()
+            : undef));
+    }
+    return @return;
+}
 
 
 
@@ -64,7 +169,16 @@ sub ARG_VALUE_MAP {};
 
 
 
-sub transform_keys {
+sub _yaarg_to_list {
+
+    return %{$_[0]} if (ref($_[0]) eq 'HASH');
+    return @{$_[0]} if (ref($_[0]) eq 'ARRAY'
+        and !(@{$_[0]} % 2));
+    return ();
+}
+
+
+sub _yaarg_transform_keys {
 
     my ($hash, $key_map, $no_dup) = @_;
     
@@ -78,12 +192,12 @@ sub transform_keys {
 }
 
 
-sub transform_values {
-    &_transform_values($_[0], $_[1], '');
+sub _yaarg_transform_values {
+    _yaarg_transform_values_r($_[1], $_[2], '');
 }
 
 
-sub _transform_values {
+sub _yaarg_transform_values_r {
 
     my ($struct, $type_map, $key) = @_;
 
@@ -92,12 +206,12 @@ sub _transform_values {
 
         when ('ARRAY') {
             return [ map {
-                &_transform_values($_, $type_map, $key);
+                _yaarg_transform_values($_, $type_map, $key);
                 } @$struct ];
         }
         default {
 
-            my $target = (defined($key) && $type_map)
+            my $target = (defined($key) and $type_map)
                 ? $type_map->{$key}
                 : undef;
             
@@ -115,7 +229,7 @@ sub _transform_values {
 
                 $target = {};
                 foreach (keys(%$struct)) {
-                    $target->{$_} = &_transform_values(
+                    $target->{$_} = _yaarg_transform_values(
                         $struct->{$_}, $type_map, $_);
                 }
                 return $target;
