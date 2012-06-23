8a22f8ddcc90062a8d8767305c446c7287be0842
A.J. Lucas <AnthonyJLucas@gmail.com>
2012-06-23 18:20:22 +0200

    - ARGS can now take a class argument, and only then falls back to caller when not supplied

--- lib/Config/YAARG.pm
+++ lib/Config/YAARG.pm
@@ -65,7 +65,7 @@ sub ARG_VALUE_TRANS {};
 
 sub ARGS {
 
-    my $class = caller();
+    my $class = $_[0] || caller();
     my $config = _yaarg_fetch_config(__PACKAGE__, $class);
     my $names = $config->{names};
     return unless ($names);
